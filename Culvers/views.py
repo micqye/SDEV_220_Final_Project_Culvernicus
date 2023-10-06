169566f80b622442a71d1467d8ca2058440089a9
micqyela <msantanaperez@ivytech.edu>
2023-10-06 14:26:23 -0400

    added database and menu items

--- Culvers/views.py
+++ Culvers/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        pass
+        #get item from each category
+        
+
+        #pass into context
+
+        #render templates
--- myvenv/Scripts/activate.bat
+++ myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ioaoe\Desktop\SDEV_220_Final\myvenv
+set VIRTUAL_ENV=C:\Users\micqy\OneDrive\Desktop\SDEV_220_Final_Project_Culvernicus\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
