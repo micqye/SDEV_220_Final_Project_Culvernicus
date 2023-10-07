3a686f9986fbfd704bba418a31a3601d084b1850
micqyela <msantanaperez@ivytech.edu>
2023-10-07 14:28:45 -0400

    added basic GUI and code for ordering -mickey

--- Culvers/urls.py
+++ Culvers/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
-from . import views
+from . import views 
+from .views import Index, Order
 
+urlpatterns = [
+    path('', Index.as_view(), name="index"),
+    path('order/', Order.as_view(), name='order'),
+
+]
--- Culvers/views.py
+++ Culvers/views.py
@@ -1,13 +1,75 @@
 from django.shortcuts import render
 from django.views import View
+from .models import MenuItem, Category, Order
 
 # Create your views here.
+class Index(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'Culvers/index.html')
+
+
+
+
 class Order(View):
     def get(self, request, *args, **kwargs):
-        pass
-        #get item from each category
         
-
+        #get item from each category
+        BUTTERBURGERS = MenuItem.objects.filter(category__name__contains='BUTTERBURGERS')
+        chicken_sandwhich = MenuItem.objects.filter(category__name__contains='Chicken & Sandwhiches')
+        seafood_salad = MenuItem.objects.filter(category__name__contains='Seafood & Salads')
+        sides = MenuItem.objects.filter(category__name__contains='Sides')
+        drinks = MenuItem.objects.filter(category__name__contains='Drinks')
+        frozen_custard = MenuItem.objects.filter(category__name__contains='Fresh Frozen Custard')
         #pass into context
+        context = {
+            'BUTTERBURGERS': BUTTERBURGERS,
+            'chicken_sandwhich': chicken_sandwhich,
+            'seafood_salad': seafood_salad,
+            'sides': sides,
+            'drinks': drinks,
+            'frozen_custard': frozen_custard,
+        }
 
         #render templates
+        return render(request, 'Culvers/order.html', context)
+    
+    #method to calculate total
+    def post(self, request, *args, **kwargs):
+        #order item dictionary with a list of items
+        order_items = {
+            'items': []
+        }
+
+        #grab all selected items
+        items = request.POST.getlist('items[]')
+        #get the item data for each selected item
+        for item in items:
+            menu_item = MenuItem.objects.get(pk=int(item))
+            item_data = {
+                'id': menu_item.pk,
+                'name':menu_item.name,
+                'price': menu_item.price
+            }
+
+            #append selected data to a list
+            order_items['items'].append(item_data)
+
+            #price and item id variables
+            price = 0
+            item_ids = []
+
+        #loop through and get the price and id
+        for item in order_items['items']:
+            price += item['price']
+            item_ids.append(item['id'])
+
+        #set price to total price
+        order = Order.objects.create(price=price)
+        order.items.add(*item_ids)
+
+        context = {
+            'items': order_items['items'],
+            'price': price
+        }
+        #go to confirmation page
+        return render(request, 'Culvers/order_confirmation.html', context)
